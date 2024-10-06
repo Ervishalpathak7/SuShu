2a13ff8a5614f144bb996de8ecd120de94775a39
Vishal Pathak <Vishalpathak.er@gmail.com>
2024-10-06 12:59:28 +0530

    test

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import MaintenancePage from './Components/maintenance-page.jsx'
 import { RouterProvider } from 'react-router-dom'
 import Router from './App'
 
