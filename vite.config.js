de1544005a94871e94a9f9ee55acc3e5064175e8
Vishal Pathak <Vishalpathak.er@gmail.com>
2024-10-13 15:24:18 +0530

    Fix routing issue and add Vercel configuration

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from 'vite'
-import path from "path"
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';      // Import Vite's defineConfig function
+import path from 'path';                  // Import the path module from Node.js
+import react from '@vitejs/plugin-react'; // Import the Vite React plugin
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react()],                    // Use the React plugin
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": path.resolve(__dirname, "./src"), // Create an alias for the src directory
     },
   },
-})
+});
